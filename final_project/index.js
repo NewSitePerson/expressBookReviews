d31fc4f56546794ccaf9cecc7728a681f3301b8f
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-08-03 20:58:11 -0400

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,11 +12,21 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
-
-return res
-    .status(401)
-    .json({ message: "Please log in to access" });
-
+let token = req.session.authorization['accessToken']; // Access Token
+        
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user; // Set authenticated user data on the request object
+                next(); // Proceed to the next middleware
+            } else {
+                return res.status(403).json({ message: "User not authenticated" }); // Return error if token verification fails
+            }
+        });
+        
+        // Return error if no access token is found in the session
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 
 });
  
