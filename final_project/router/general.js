c77512ac7a6821ec949c958865473b4852dd4a1d
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-08-04 01:06:53 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,7 +31,16 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
+  let booksbytitle = [];
+  let isbns = Object.keys(books);
+  isbns.forEach((isbn) => {
+    if(books[isbn]["title"] === req.params.title) {
+      booksbytitle.push({"isbn":isbn,
+                          "author":books[isbn]["author"],
+                          "reviews":books[isbn]["reviews"]});
+    }
+  });
+  res.send(JSON.stringify({booksbytitle}, null, 4));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
