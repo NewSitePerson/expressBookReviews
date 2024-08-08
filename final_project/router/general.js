2c33218ddbbbeb8a088c713afa790b50ee0be94c
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-08-07 22:38:31 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,17 +6,18 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-   
-  if (!username || !password) {
-      return res.status(400).json({ message: "Username and password are required" });
-    }
+    const username = req.body.username;
+    const password = req.body.password;
   
-    if (users.hasOwnProperty(username)) {
-      return res.status(409).json({ message: "Username already exists" });
-    }
-  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User" +username+ "successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User" +username+ " already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
     // Assuming you have a function to hash passwords
     const hashedPassword = hashPasswordFunction(password);
   
@@ -54,7 +55,21 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
+    const isbnParam = req.params.isbn;
+
+    // Get the book reviews based on ISBN provided in the request parameters
+
     const reviews = books[isbnParam]["reviews"];
+
+    if (!reviews) {
+
+      res.status(404).json({ message: 'No reviews found for the ISBN provided' });
+
+    } else {
+
+      res.status(200).json(reviews);
+
+    }
 });
 
 module.exports.general = public_users;
